87ddbcb85c129704be7bf2b526d3d0f88b6c6b81
raventot <36997228+raventot@users.noreply.github.com>
2021-07-01 14:36:43 +0800

    Add files via upload

---
+++ db_connect.php
@@ -0,0 +1,3 @@
+<?php 
+
+$conn= new mysqli('localhost','root','','grab_db')or die("Could not connect to mysql".mysqli_error($con));
---
+++ login.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php 
+session_start();
+include('./db_connect.php');
+  ob_start();
+    $system = $conn->query("SELECT * FROM system_settings")->fetch_array();
+    foreach($system as $k => $v){
+      $_SESSION['system'][$k] = $v;
+    }
+  ob_end_flush();
+?>
+<?php 
+if(isset($_SESSION['login_id']))
+header("location:index.php?page=home");
+
+?>
+<?php include 'header.php' ?>
+<body class="hold-transition login-page">
+<div class="login-box">
+  <div class="login-logo">
+    <h3><b><font color="#ff1493" face="arial">GRABA</font><font color="#ffffff" face="arial">TRUCK</font><font color="cyan" face="arial">LOAD</font></b></h3>
+    <font color="#B5B5C3" face="Poppins" size="4">Login to your account</font>
+  </div>
+  <div class="card">
+    <div class="card-body login-card-body">
+      <form action="" id="login-form">
+        <div class="input-group mb-3">
+          <input type="email" class="form-control" name="email" required placeholder="Email">
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-envelope"></span>
+            </div>
+          </div>
+        </div>
+        <div class="input-group mb-3">
+          <input type="password" class="form-control" name="password" required placeholder="Password">
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-lock"></span>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-8">
+            <div class="icheck-primary">
+              <input type="checkbox" id="remember">
+              <label for="remember">
+                Remember Me
+              </label>
+            </div>
+          </div>
+
+          <div class="col-4">
+            <button type="submit" class="btn btn-primary btn-block">Login</button>
+          </div>
+        </div>
+      </form>
+    </div>
+  </div>
+</div>
+<script>
+  $(document).ready(function(){
+    $('#login-form').submit(function(e){
+    e.preventDefault()
+    start_load()
+    if($(this).find('.alert-danger').length > 0 )
+      $(this).find('.alert-danger').remove();
+    $.ajax({
+      url:'ajax.php?action=login',
+      method:'POST',
+      data:$(this).serialize(),
+      error:err=>{
+        console.log(err)
+        end_load();
+
+      },
+      success:function(resp){
+        if(resp == 1){
+          location.href ='index.php?page=home';
+        }else{
+          $('#login-form').prepend('<div class="alert alert-danger">Username or password is incorrect.</div>')
+          end_load();
+        }
+      }
+    })
+  })
+  })
+</script>
+<?php include 'footer.php' ?>
+
+</body>
+</html>
